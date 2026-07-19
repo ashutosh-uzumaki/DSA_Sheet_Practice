56f09d291959b64451e4e36941ef37f4ccb2085a
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2026-07-19 11:59:09 +0530

    solved partition array into three equal parts

---
+++ src/main/java/org/example/dynamic_programming/partition_dp/three_equal_parts/PartitionArrayIntoThreeEqualParts.java
@@ -0,0 +1,25 @@
+package org.example.dynamic_programming.partition_dp.three_equal_parts;
+
+public class PartitionArrayIntoThreeEqualParts {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int totalSum = Arrays.stream(arr).sum();
+        if(totalSum % 3 != 0){
+            return false;
+        }
+        int target = totalSum / 3;
+
+        int partsFound = 0;
+        int runningSum = 0;
+        for(int i=0; i<arr.length; i++){
+            runningSum += arr[i];
+            if(runningSum == target){
+                partsFound += 1;
+                runningSum = 0;
+                if(partsFound == 2 && i < arr.length - 1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
